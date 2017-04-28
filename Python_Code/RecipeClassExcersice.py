91c2392ba0121475745a0dbdb3dda57912e364e8
Tessu <henrithessler@gmail.com>
2017-04-28 14:44:00 +0300

    uiii

--- Python_Code/RecipeClassExcersice.py
+++ Python_Code/RecipeClassExcersice.py
@@ -7,12 +7,16 @@ class Recipe(object):
 		self.ingredient1 = ingredient1
 		self.ingredient2 = ingredient2
 		self.ingredient3 = ingredient3
+	def __repr__(self):
+		return ", ".join(self.keyword, self.ingredient1, self.ingredient2, self.ingredient3)
 
 recipe1 = Recipe("chickensoup", "chicken", "carrot", "potatoes")
 recipe2 = Recipe("vegetablesoup", "carrot", "potatoes", "cucumber")
 recipe3 = Recipe("beefstew", "beef", "mushroom", "onion")
 recipe4 = Recipe("dessert", "chocolate", "flour", "sugar")
 recipe5 = Recipe("pizza", "beef", "tuna", "pineapple")
+
+
 
 
 class RecipeManager(object):
@@ -26,7 +30,11 @@ class RecipeManager(object):
 
 	def add_recipe_uncategorized(self, rec):
 		self.uncategorized_dishes.append(rec)
-	def add_beef_dish(self,dish):
+
+	def add_chicken_dish(self, dish):
+		self.chicken_dishes.append(dish)
+
+	def add_beef_dish(self, dish):
 		self.beef_dishes.append(dish)
 
 	def add_vegetable_dish(self,dish):
@@ -52,8 +60,21 @@ class RecipeManager(object):
 	def get_desserts(self):
 		return desserts
 
+
+
 	def add_recipe(self):
 		for recipe in self.uncategorized_dishes:
+			if "beef" in recipe:
+				recipemanager.add_beef_dish(recipe)
+			if "vegetable" in recipe:
+				recipemanager.add_vegetable_dish(recipe)
+			if "dessert" in recipe:
+				recipemanager.add_dessert(recipe)
+			if "soup" in recipe:
+				recipemanager.add_soup(recipe)
+			if "chicken" in recipe:
+				recipemanager.add_chicken_dish(recipe)
+
 
 
 
@@ -64,3 +85,6 @@ recipemanager.add_recipe_uncategorized(recipe2)
 recipemanager.add_recipe_uncategorized(recipe3)
 recipemanager.add_recipe_uncategorized(recipe4)
 recipemanager.add_recipe_uncategorized(recipe5)
+
+
+print recipemanager.get_desserts
